56bc970c1b86dcfc1b7e334e0e5038fe7666cdfa
acela <acela@EYZ>
2024-04-04 16:25:35 +0800

    Add project files.

---
+++ Calculator 2.0 (Midterm)/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Calculator_2._0__Midterm_.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Calculator_2._0__Midterm_.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Calculator 2.0 (Midterm)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Calculator_2._0__Midterm_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
